4d0661b407cefb7d374b481af2e28e54b91cc96d
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-05-28 14:06:33 +0900

    context 제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import TextInput from "./components/TextInput";
 import TextList from "./components/TextList";
+
+//1. 함수 외부 context 생성
+export const TextContext = createContext();
 
 function App() {
   // TODO: texts 를 context api 로 리팩터링 하세요.
   const [texts, setTexts] = useState(() =>
     localStorage.getItem("texts")
       ? JSON.parse(localStorage.getItem("texts"))
-      : [],
+      : []
   );
 
   useEffect(() => {
@@ -19,11 +22,14 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Text Input and Listing</h1>
-      <TextInput onAddText={onAddText} />
-      <TextList texts={texts} />
-    </div>
+    //2. 값 전달
+    <TextContext.Provider value={{ texts, onAddText }}>
+      <div>
+        <h1>Text Input and Listing</h1>
+        <TextInput />
+        <TextList />
+      </div>
+    </TextContext.Provider>
   );
 }
 
